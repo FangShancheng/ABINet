e0c73e89ee76a1f891cacfe7951a998473779ac9
Seonwhee-Genome <quarklep@naver.com>
2022-01-19 11:27:29 +0900

    modified the file in the case where the image file name is the label

--- tools/create_lmdb_dataset_var.py
+++ tools/create_lmdb_dataset_var.py
@@ -1,4 +1,4 @@
-""" a modified version of CRNN torch repository https://github.com/bgshih/crnn/blob/master/tool/create_dataset.py """
+"""A modification of create_lmdb_dataset.py in the case where gt label is the name of the image file"""
 
 import fire
 import os
@@ -6,52 +6,34 @@ import lmdb
 import cv2
 
 import numpy as np
+from glob import glob
+from create_lmdb_dataset import checkImageIsValid, writeCache
 
 
-def checkImageIsValid(imageBin):
-    if imageBin is None:
-        return False
-    imageBuf = np.frombuffer(imageBin, dtype=np.uint8)
-    img = cv2.imdecode(imageBuf, cv2.IMREAD_GRAYSCALE)
-    imgH, imgW = img.shape[0], img.shape[1]
-    if imgH * imgW == 0:
-        return False
-    return True
-
-
-def writeCache(env, cache):
-    with env.begin(write=True) as txn:
-        for k, v in cache.items():
-            txn.put(k, v)
-
-
-def createDataset(inputPath, gtFile, outputPath, checkValid=True):
+def createDataset(inputPath, outputPath, checkValid=True):
     """
     Create LMDB dataset for training and evaluation.
     ARGS:
         inputPath  : input folder path where starts imagePath
-        outputPath : LMDB output path
-        gtFile     : list of image path and label
+        outputPath : LMDB output path        
         checkValid : if true, check the validity of every image
     """
     os.makedirs(outputPath, exist_ok=True)
     env = lmdb.open(outputPath, map_size=1099511627776)
     cache = {}
     cnt = 1
+    
+    datalist = glob(os.path.join(inputPath, "*.jpg"))  # create a list containing image file path
+    labelList = list(map(lambda x: x.split('/')[-1].replace('.jpg', ''),datalist)) # create a list that contains the label corresponding filePath
 
-    with open(gtFile, 'r', encoding='utf-8') as data:
-        datalist = data.readlines()
 
     nSamples = len(datalist)
-    for i in range(nSamples):
-        #imagePath, label = datalist[i].strip('\n').split('\t') 
-        imagePath, label = datalist[i].strip('\n').split(' ')
-        imagePath = os.path.join(inputPath, imagePath)
-
-        # # only use alphanumeric data
-        # if re.search('[^a-zA-Z0-9]', label):
-        #     continue
-
+    for imagePath, label in zip(datalist, labelList):
+        """
+        imagePath label 예시
+        /home/ubuntu/Dataset/text_recognition/Korean/public_crop/이해.jpg 이해
+        """
+    
         if not os.path.exists(imagePath):
             print('%s does not exist' % imagePath)
             continue
@@ -71,7 +53,7 @@ def createDataset(inputPath, gtFile, outputPath, checkValid=True):
         imageKey = 'image-%09d'.encode() % cnt
         labelKey = 'label-%09d'.encode() % cnt
         cache[imageKey] = imageBin
-        cache[labelKey] = label.encode()
+        cache[labelKey] = label.encode('utf-8')
 
         if cnt % 1000 == 0:
             writeCache(env, cache)
