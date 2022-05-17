ec4f7db4b4081b50751bdedfb2b2677d3aaed154
Seonwhee-Genome <quarklep@naver.com>
2022-05-17 16:31:17 +0900

    Modified for KoreanSTR dataset

--- tools/create_lmdb_dataset_KoreanSTR.py
+++ tools/create_lmdb_dataset_KoreanSTR.py
@@ -4,45 +4,110 @@ import fire
 import os
 import lmdb
 import cv2
-import re
+import re, json
 
 import numpy as np
+import pandas as pd
+
+from PIL import Image 
+from matplotlib import pyplot as plt
 from glob import glob
 from create_lmdb_dataset import checkImageIsValid, writeCache
+from create_lmdb_dataset_var import get_hangul
+
+from typing import List, Dict, Tuple
+
+root_path = '/Data2/Dataset/KoreanSTR/'
+
+class KoreanSTRset:
+    
+    def __init__(self, labelDict):
+        '''        
+        ImageDict 
+        e.g : [...{'id': '00000003',
+             'width': 3739,
+             'height': 175,
+             'file_name': '00000003.png',
+             'license': 'AI 오픈 이노베이션 허브'},
+            ...], 
+        
+        AnnoDicts 
+        e.g : [...{'id': '00000003',
+                  'image_id': '00000003',
+                  'text': '정권자였는데 이에 대한 사과가 우선"이라고 했다. 한편 문 후보와 김',
+                  'attributes': {'type': '문장', 'gender': '여', 'age': '28', 'job': '직장인'}},
+              ...], 
+        '''
+        labelDict: Dict[str, List]
+        ImageDicts: List[Dict[str, str]] = labelDict['images']
+        AnnoDicts: List[Dict[str, str]] = labelDict['annotations']            
+        
+        self.img_dir = {'글자(음절)': '1_syllable', '문장': '1_sentence', '단어(어절)': '1_word' }
+        
+        self.AnnotDF = pd.DataFrame(AnnoDicts)
+        self.ImgDF = pd.DataFrame(ImageDicts)
+        self.file_annot_pair = []
+        del ImageDicts, AnnoDicts
+        
+        
+    def imgView(self, filePath: str):
+        '''
+        filePath : complete path of single image file
+        '''
+        ImgArray = np.array(Image.open(filePath), dtype=np.uint8)
+        plt.figure(figsize=(24, 12))
+        ax1 = plt.subplot(111)
+        ax1.imshow(ImgArray)
+        plt.show()
+        
+    
+    def imgName2path_annot(self, fileName: str, toView:bool = False) -> Tuple[str, str]:
+        '''
+        toView     matplotlib으로 이미지 시각화할 것인지 확인하는 옵션
+        
+        annot : pd.Series
+        
+        |  |   id     | image_id |                                  text                             |              attributes                 |
+        | 1| 00000003 | 00000003 | 정권자였는데 이에 대한 사과가 우선"이라고 했다. 한편 문 후보와 김 | {'type': '문장', 'gender': '여',... }   |
+        
+        imgID : pd.Series
+        
+        |  |   id     |  width  | height |     file_name      |              license           |
+        | 1| 00000003 |  3739   |  175   |    00000003.png    |     AI 오픈 이노베이션 허브    |
+        
+        '''
+        imgID = self.ImgDF[self.ImgDF['file_name']==fileName]['id'].values[0]
+        annot = self.AnnotDF[self.AnnotDF['id']==imgID]
+        annot_text = annot['text'].values[0]
+        annot_type = annot['attributes'].values[0]['type']
+        imgFile = os.path.join(root_path + self.img_dir[annot_type], fileName)
+        if toView:
+            self.imgView(imgFile)
+        return imgFile, annot_text
+    
+    
+    def get_all_pairs(self) -> List[Tuple[str, str]]:
+        
+        total_imgs : List[str] # all the paths of available png files
+        total_imgs_file : List[str]  # list of img file names ['00022234.png', '00000011.png', ..]
+        img_annot_pairs : List[Tuple[str, str]] # list of image path - annotation pairs  [('/Data2/Dataset/KoreanSTR/1_syllable/00022234.png', '몜'), ...]
+        
+        total_imgs = []
+        img_annot_pairs = []
+        for folder in list(self.img_dir.values()):
+            total_imgs += glob( root_path + folder + '/*.png')
+            
+        total_imgs_file = list(map(lambda x: x.split('/')[-1], total_imgs))
+        del total_imgs
+        
+        img_annot_pairs += list(map(lambda imgQuery: self.imgName2path_annot(imgQuery), total_imgs_file[:10000]))  ## 실행되는지 확인하기 위해 100개만
+        return img_annot_pairs
+        
 
 
-def get_hangul(inputLabel):
-    '''transform hangul character into 3 syllables
-    Args :
-        inputLabel : str
-    Return :
-        자모 분리해서 합친 뒤 공백(종성이 없는 경우)을 제거 : str
-    '''
-    base_unicode, initial_unicode, medium_unicode = 44032, 588, 28
-    initial_list = list('ㄱㄲㄴㄷㄸㄹㅁㅂㅃㅅㅆㅇㅈㅉㅊㅋㅌㅍㅎ')
-    medium_list = list('ㅏㅐㅑㅒㅓㅔㅕㅖㅗㅘㅙㅚㅛㅜㅝㅞㅟㅠㅡㅢㅣ')
-    terminal_list = list(' ㄱㄲㄳㄴㄵㄶㄷㄹㄺㄻㄼㄽㄾㄿㅀㅁㅂㅄㅅㅆㅇㅈㅊㅋㅌㅍㅎ') # 종성이 없는 경우도 있으므로 공백이 필요하다
-    labelList = list(inputLabel)
-    split_result = []
-    for keyword in labelList:
-        if re.match('.*[가-힣]+.*', keyword) is not None:
-            # 초성
-            char_code = ord(keyword) - base_unicode
-            init_char = int(char_code/initial_unicode)
-            split_result.append(initial_list[init_char])
-            # 중성
-            med_char = int((char_code - (initial_unicode*init_char))/medium_unicode)
-            split_result.append(medium_list[med_char])
-            # 종성
-            term_char = int((char_code - (initial_unicode*init_char)-(medium_unicode*med_char)))
-            split_result.append(terminal_list[term_char])
-        else:
-            split_result.append(keyword)
-    output = "".join(split_result)
-    return output.replace(' ', '')
 
 
-def createDataset(inputPath, outputPath, checkValid=True):
+def createDataset(outputPath, checkValid=True):
     """
     Create LMDB dataset for training and evaluation.
     ARGS:
@@ -50,20 +115,24 @@ def createDataset(inputPath, outputPath, checkValid=True):
         outputPath : LMDB output path        
         checkValid : if true, check the validity of every image
     """
+    
+    with open("/Data2/Dataset/KoreanSTR/handwriting_data_info1.json", "r", encoding='utf-8') as json_file:
+        KorlabelDict = json.load(json_file)
+    
+    imgAnnot = KoreanSTRset(KorlabelDict)
+    img_annot_pairs = imgAnnot.get_all_pairs()
+    
     os.makedirs(outputPath, exist_ok=True)
     env = lmdb.open(outputPath, map_size=1099511627776)
     cache = {}
     cnt = 1
-    
-    datalist = glob(os.path.join(inputPath, "*.jpg"))  # create a list containing image file path
-    labelList = list(map(lambda x: x.split('/')[-1].replace('.jpg', ''),datalist)) # create a list that contains the label corresponding filePath
 
 
-    nSamples = len(datalist)
-    for imagePath, label in zip(datalist, labelList):
+    nSamples = len(img_annot_pairs)
+    for imagePath, label in img_annot_pairs:
         """
-        imagePath label 예시 : /home/ubuntu/Dataset/text_recognition/Korean/public_crop/이해.jpg 이해
-        이해 --> ㅇㅣㅎㅐ
+        imagePath label 예시 : '/Data2/Dataset/KoreanSTR/1_syllable/00022234.png' '몜'
+        몜 -> ㅁㅖㅁ
         """
     
         if not os.path.exists(imagePath):
